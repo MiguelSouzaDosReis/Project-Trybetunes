4c5010644907a4188e89a3736231a17d25c52ba3
Miguel_Reis <guelsouzar@gmail.com>
2021-10-26 13:19:48 -0300

    requisto 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      objeto: {},
+      carregando: false,
+    };
+  }
+
+  componentDidMount() {
+    this.BuscaUsuario();
+  }
+
+  BuscaUsuario = () => {
+    this.setState({ carregando: true },
+      () => getUser().then((response) => {
+        this.setState({ objeto: response, carregando: false });
+      }));
+  }
+
   render() {
+    const { objeto, carregando } = this.state;
     return (
-      <header data-testid="header-component" { ...getUser() }>
+      <header data-testid="header-component">
+        <span data-testid="header-user-name">
+          {objeto.name}
+        </span>
+        {carregando && <Carregando />}
         <Link data-testid="link-to-search" to="/search">Search</Link>
         <Link data-testid="link-to-favorites" to="/favorites"> Favoritos </Link>
         <Link data-testid="link-to-profile" to="/profile"> Profile </Link>
