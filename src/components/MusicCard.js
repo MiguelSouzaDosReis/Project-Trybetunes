efd6c13c883d5c6998a0e958304c48ea94aafe68
Miguel_Reis <guelsouzar@gmail.com>
2021-10-27 21:23:02 -0300

    requisito 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Carregando from './Carregando';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      carregando: false,
+      check: false,
+    };
+  }
+
+  AdicionarMusica = async () => {
+    const { element } = this.props;
+    this.setState({ carregando: true });
+    await addSong({ element });
+    this.setState({ carregando: false, check: true });
+  }
+
   render() {
     const { element } = this.props;
+    const { carregando, check } = this.state;
     return (
       <div>
+        {carregando && <Carregando /> }
+
         <p>{element.trackName}</p>
         <audio data-testid="audio-component" src={ element.previewUrl } controls>
           <track kind="captions" />
@@ -13,6 +33,18 @@ class MusicCard extends React.Component {
           <code>audio</code>
           .
         </audio>
+
+        <label htmlFor={ element.trackId }>
+
+          <input
+            data-testid={ `checkbox-music-${element.trackId}` }
+            type="checkbox"
+            checked={ check }
+            onChange={ this.AdicionarMusica }
+          />
+          {' '}
+          Favorita
+        </label>
       </div>
 
     );
@@ -22,6 +54,7 @@ MusicCard.propTypes = {
   element: PropTypes.shape({
     trackName: PropTypes.string,
     previewUrl: PropTypes.string,
+    trackId: PropTypes.number,
   }).isRequired,
 };
 
